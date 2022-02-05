f66f1cbeddd42689f88812274055703b1c847bc6
keshavdulal <Keshav.dulal@gmail.com>
2022-02-05 21:42:31 +0545

    add calculation logic

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -1,5 +1,5 @@
 import { IonicModule } from '@ionic/angular';
-import { NgModule, Input, OnInit } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
@@ -17,30 +17,4 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
   ],
   declarations: [Tab1Page],
 })
-export class Tab1PageModule implements OnInit {
-  // title: string = 'Pay Calculatorr';
-  // title = 'Pay Calculatorrr';
-
-  // input
-  hoursWorked;
-  hourlyRate;
-
-  // output
-  hourlyPay;
-  regularPay;
-  overtimePay;
-  totalPay;
-  tax;
-
-  constructor() {}
-
-  ngOnInit(): void {}
-
-  calculatePay() {
-    console.log('@calculatePay');
-    // console.log(this.hoursWorked);
-    // console.log(this.hourlyRate);
-
-    // this.hourlyPay = this.hourlyRate;
-  }
-}
+export class Tab1PageModule {}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  title: string = 'Pay Calculator';
+
+  // input
+  hoursWorked;
+  hourlyRate;
+
+  // output
+  regularPay = 0;
+  overtimePay = 0;
+  totalPay = 0;
+  tax = 0;
+  netPay = 0; // cash in hand
 
   constructor() {}
 
+  ngOnInit(): void {}
+
+  calculatePay() {
+    console.log('@calculatePay', this.hoursWorked, this.hourlyRate);
+
+    if (!this.hourlyRate || !this.hourlyRate) {
+      console.log('Enter Params');
+      return;
+    }
+
+    if (this.hoursWorked <= 40) {
+      this.regularPay = this.hoursWorked * this.hourlyRate;
+      this.overtimePay = 0; // no overtime at all
+    } else {
+      // more than 40hrs
+      this.regularPay = 40 * this.hourlyRate;
+      this.overtimePay = (this.hoursWorked - 40) * this.hourlyRate * 1.5;
+    }
+
+    // common checks for both type
+    this.totalPay = this.regularPay + this.overtimePay;
+    this.tax = this.totalPay * 0.18;
+    this.netPay = this.totalPay - this.tax;
+  }
 }
